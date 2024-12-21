32073b3a39174b6dae20f4a5a6eafab89260d08d
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-21 17:41:36 +0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Kevin Darmawan on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
