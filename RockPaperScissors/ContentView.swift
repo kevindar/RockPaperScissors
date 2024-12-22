6d435ae8dfb73d5a32e155f2881b5fd8bf1176be
Kevin Darmawan <kevindar2705@gmail.com>
2024-12-23 00:00:31 +0700

    First commit. Up to Day 25

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    // game states
+    let totalRounds = 10
+    @State private var round: Int = 0
+    @State private var score: Int = 0
+    
+    @State var options = ["rock", "paper", "scissors"]
+    @State var cpuOption = Int.random(in: 0...2)
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
